bfd5f89d8032211eb20736a0e33c281a9ee8238f
Kakha1998 <kakha.gujejiani.1@iliauni.edu.ge>
2020-08-18 14:33:52 +0400

    Updates

---
+++ precache-manifest.9e870820c24e6d89a32034e6b03eefe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620bab43906a5fb94e8db279f89c9db9",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "42a6ce64ba2267294b79",
+    "url": "/markdown-previewer/static/css/main.5f2f3d5d.chunk.css"
+  },
+  {
+    "revision": "8e3dfca05a0d7fba4743",
+    "url": "/markdown-previewer/static/js/2.23e9067d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.23e9067d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a6ce64ba2267294b79",
+    "url": "/markdown-previewer/static/js/main.fc3ff9ba.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
